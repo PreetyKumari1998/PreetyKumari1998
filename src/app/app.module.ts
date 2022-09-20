d676ee56f0a89f70b6508d1c5557fce29d83eaa7
ext_kumari <itsoftware4@stulzservice.in>
2022-09-20 12:20:04 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { QuestionComponent } from './components/question/question.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ChangeBgDirective } from './directives/change-bg.directive';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    QuestionComponent,
+    HeaderComponent,
+    ChangeBgDirective,
+   
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
